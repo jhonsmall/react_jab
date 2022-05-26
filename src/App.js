6320f57a7b1e0071d4085f7e556e6b204186a63d
Juan Chico <jchico@mushucruna.com>
2022-05-26 18:11:57 -0500

    lista la suma

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
-import './miCss.css';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [nombre, setNombre] = useState('Rey');
-  // let nombre = 'rey';
-  const cambiar = () => {
-    setNombre('Juan');
-    console.log(nombre);
-  } 
+  let [numero1, setNumero1] = useState(0);
+  let [numero2, setNumero2] = useState(0);
+  let [resultado, setResultado] = useState(0);
+  const sumar = () => {
+    setResultado(Number(numero1) + Number(numero2));
+  };
+
+  const modificar = (e) => {
+    if (e.target.name === 'numero1') {
+      setNumero1(e.target.value);
+    } else {
+      setNumero2(e.target.value);
+    }
+  };
+
   return (
-    <div className="contenedor">
-      <h1>Me llamo {nombre}</h1>
-      <button onClick={cambiar}>Cambiar Nombre</button>
+    <div className="caja">
+      <input type="number" name='numero1' value={numero1} onChange={modificar} /> +
+      <input type="number" name='numero2' value={numero2} onChange={modificar} /> =
+      <input type="number" value={resultado} readOnly />
+      <button onClick={sumar}>Sumar</button>
     </div>
   );
 }
